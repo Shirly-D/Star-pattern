62c2c65b4c11873a0f306abc75d747a2be3cb848
Shirly <shirlyd@axioned.com>
2021-07-22 13:39:35 +0530

    js initial

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,22 @@
 
 */
 
+// 1.
+
+let n = 5;
+let string = "";
+for (let i = 1; i <= n; i++) {
+    for (let j = 1; j < i; j++) {
+        string = string + "";
+    }
+
+    for (k = 1; k < 2 - i; k++) {
+        string = string + "*";
+    }
+    string = string + "\n";
+}
+console.log(string);
+
 
 
 
